0f8ab7f9f35b45cc953480a360d322aa811705d1
jorge <jubior777@hotmail.com>
2025-04-20 13:40:09 -0500

    Nuevos cambios al curso

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { API_URL } from '../env'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <h1>Hello World Day</h1>
+        <h1>Hello World Day{API_URL}</h1>
       </div>
       
     </>
