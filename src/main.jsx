4810551273b8dba550129916b4ede646950171a6
jorge <jubior777@hotmail.com>
2025-04-27 18:27:01 -0500

    se agrega stylos

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./Router/router"; 
-import './index.css'// Asegúrate de que la ruta sea correcta
+import './styles/index,css'// Asegúrate de que la ruta sea correcta
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
